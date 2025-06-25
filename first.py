e956caf59854e68d3bbb5e58847fe677461600ab
master <gutislgh@yaho>
2025-06-25 11:43:45 +0300

    Init commit

---
+++ first.py
@@ -0,0 +1,22 @@
+prompt = """Left or Forward or Right"""
+print(prompt)
+choice = input('Choice: ')
+if choice == 'L' or choice == 'l':
+    print('Wave')
+elif choice == 'F' or choice == 'f':
+    print('Yahoo')
+elif choice == 'R' or choice == 'r':
+    print('Horse')
+else:
+    print('Not choice')
+
+
+'''
+a = 3
+if a == 5:
+    print('ura')
+elif a == 3:
+    print('yahoo')
+else:
+    print('not')
+'''
