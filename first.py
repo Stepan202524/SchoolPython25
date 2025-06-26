e1b323f14483e944fa42c128f2d81972265a4820
master <gutislgh@yaho>
2025-06-26 10:53:52 +0300

    Square Eq

--- first.py
+++ first.py
@@ -1,9 +1,17 @@
-# Iterable object
-# len() - сколько элементов в объекте
-a = 123456  # int - не является iterable
-lenght = len(str(a))  # поэтому конвертируем в str
-print(lenght)
+a = int(input('A='))
+b = int(input('B='))
+c = int(input('C='))
+if a != 0:
+    d = b ** 2 - 4 * a * c
+    if d < 0:
+        print('No korni')
+    elif d == 0:
+        x = -b / (2 * a)
+        print(f'Koren: {x:.2f}')
+    else:
+        x1 = (-b + d ** 0.5) / 2 * a
+        x2 = (-b - d ** 0.5) / 2 * a
+        print(f'Korni:\n\tx1 = {x1:.2f}\n\tx2 = {x2:.2f}\n\tx2')
+else:
+    print('A>0')
 
-word = input(' please word for analys:')
-if not word or len(word) < 3:
-    print('Net slova or small')
