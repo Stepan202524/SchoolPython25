cfddca97efcddb4f94397acf20fa913a0a8b2c00
master <gutislgh@yaho>
2025-06-26 15:50:12 +0300

    Вложенные циклы

--- first.py
+++ first.py
@@ -1,20 +1,7 @@
-# min, max, average, summ, production
-total = 0
-N = 3
-prod = 1
-min_val = float('inf')  # + бесконечность
-max_val = float('-inf')  # - бесконечность
-for _ in range(N):
-    num = int(input('Please number: '))
-    total += num
-    average = total / N
-    prod *= num
-    if num < min_val:
-        min_val = num
-    if num > max_val:
-        max_val = num
-print(f'Summa: {total}')
-print(f'Sr. arifmet: {average}')
-print(f'Proizved: {prod}')
-print(f'Min: {min_val}')
-print(f'Max: {max_val}')
+# Вложенные циклы
+p = 0
+for i in range (1, 10):
+    for j in range (1, 10):
+        p = i * j
+        print(f'{i} * {j} = {p}', end='\n\t')
+    print()
