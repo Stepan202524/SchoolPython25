3906e791c04d7aa48e5b8674c415555366de625e
master <gutislgh@yaho>
2025-06-26 12:21:41 +0300

    Loop Stop Signal без моржа

--- first.py
+++ first.py
@@ -1,8 +1,7 @@
-# Loop's: (циклы)
-#print('Enter word >3 symbols')
-word = input('here:')
-# :=  оператор морж
-# while len(word := input('Plese word')) <= 3:
-while len(word) <= 3:
-    print('Try again')
-print(f'Slovo - "{word}"')
+# цикл до пустой строки
+# без :=
+word = input('please word:')
+while word != '':
+    print(f'Slovo: "{word}"')
+    word = input('Try again:')
+print('Empty stroka')
