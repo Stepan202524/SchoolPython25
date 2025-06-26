80905c24397a12d6fd425efc6c50b5b1121d21de
master <gutislgh@yaho>
2025-06-26 12:12:50 +0300

    Loop Continue морж

--- first.py
+++ first.py
@@ -1,12 +1,8 @@
 # Loop's: (циклы)
-# while
-# for
-counter = 0  # обнуляем счётчик
-# цикл из 5ти итераций
-while counter < 5:
-    print(f'Iteration number {counter}')
-    # counter = counter + 1  # инкремент
-    counter += 1 # краткая запись бинарным оператором
-while counter > 0:
-    print(f'Деитерация {counter}')
-    counter -= 1  #декремент
+#print('Enter word >3 symbols')
+word = input('here:')
+# :=  оператор морж
+# while len(word := input('Plese word')) <= 3:
+while len(word) <= 3:
+    print('Try again')
+print(f'Slovo - "{word}"')
