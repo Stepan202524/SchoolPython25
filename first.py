7740ce81a397ed0528dd63afa21110a44febeea0
master <gutislgh@yaho>
2025-06-26 11:53:23 +0300

    Loop Begin

--- first.py
+++ first.py
@@ -1,17 +1,12 @@
-a = int(input('A='))
-b = int(input('B='))
-c = int(input('C='))
-if a != 0:
-    d = b ** 2 - 4 * a * c
-    if d < 0:
-        print('No korni')
-    elif d == 0:
-        x = -b / (2 * a)
-        print(f'Koren: {x:.2f}')
-    else:
-        x1 = (-b + d ** 0.5) / 2 * a
-        x2 = (-b - d ** 0.5) / 2 * a
-        print(f'Korni:\n\tx1 = {x1:.2f}\n\tx2 = {x2:.2f}\n\tx2')
-else:
-    print('A>0')
-
+# Loop's: (циклы)
+# while
+# for
+counter = 0  # обнуляем счётчик
+# цикл из 5ти итераций
+while counter < 5:
+    print(f'Iteration number {counter}')
+    # counter = counter + 1  # инкремент
+    counter += 1 # краткая запись бинарным оператором
+while counter > 0:
+    print(f'Деитерация {counter}')
+    counter -= 1  #декремент
