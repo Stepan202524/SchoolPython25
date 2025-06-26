c22a00d8cb5f780ba638b6921ddae7cd8d84e666
master <gutislgh@yaho>
2025-06-26 14:38:04 +0300

    Keyword "in"

--- first.py
+++ first.py
@@ -1,6 +1,4 @@
-# вывод чисел, заканчивается на 3
-cout = 1
-while cout <= 100:
-    if cout % 10  == 3:
-        print(cout, end=', ')
-    cout += 1
+# ключевое слово in
+word = 'potok'
+if 'tok' in word:
+    print('yes')
