78b1b753c8396988d78c681f4d7312e181fbfe72
master <gutislgh@yaho>
2025-06-27 15:22:41 +0300

    Строки Таблицы символов

--- first.py
+++ first.py
@@ -1,22 +1,11 @@
 # Строки (immutable, iterable)
-s = 'Python'
-print(s[0])  # вывод значения по индексу, индекс может быть отрицательным (отсчёт с конца строки)
-print(f'Dlina stroki: {len(s)}')
-v = 0
-for ch in s:
-    if ch in {'a', 'e', 'i', 'o', 'y'}:  # или if ch in 'aeioy'
-        v += 1
-print(f' Number glasnyx v stroke "{s}" = {v}')
+# Таблица символов
+s ='\xB0'
+u = '\u2603' # в 16тиричной системе
 
-# перебор строки по  числовому индексу
-for index in range(len(s)):
-    print(s[index])
-# замена символа в строке по индексу
-d = 'sabaka'
-res = ''
-for i in range(len(d)):
-    if i == 1:
-        res += 'o'
-    else:
-        res += d[i]
-print(res)
+print(u)
+print('25' + s + 'C')
+print(f'Kod symbola ☃ UNICODE: {ord('☃')}')
+# ord(символ) - возвращает код символа в unicod в 10тиричной
+# chr(kod)    - возвращает символ по unicode в 10тиричной
+print(chr(9731))
