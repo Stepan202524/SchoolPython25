fb82f7e222692f75d35a3c8a6529269c62612487
master <gutislgh@yaho>
2025-06-30 15:55:15 +0300

    Unpack

--- first.py
+++ first.py
@@ -1,9 +1,19 @@
-# Кортеж Tuple
-BLACK = (0, 0, 0) # неизменяемая структура в памяти
-empty = () #tuple()
-one = (1,) # кортеж заканчивается на ,
-s = 'Python'
-t = tuple(s) + ('.',)  # создание нового кортежа прибавлением ещё одного кортежа
-print(t)
+# Кортеж (tuple, immutable)
+channels = ('red', 'green', 'blue')
+r, g, b = channels    # распаковка
+print(r, b)
+a, b, c = 1, 2, 3 # input(), input(), input()
+d, e = [4, 5], 6 # упаковка в d [4, 5]
 
-cards = [(7, 'chervy'), ('tuz', 'piki')] # кортеж из двух списков
+# студент и средний балл
+N=3
+studs = []
+for _ in range(N):
+    stud, aver = input('Name'), float(input('Ball'))
+    studs.append((stud, aver)) # append идёт кортежом (скобки в скобках) через ,
+print(studs)
+
+for st in studs:
+    stud, aver = st  # распаковка
+    print('Sudent: ', stud)
+    print('Sredniy ball: ', aver)
