58f0cfee6b3507df313f90a964046e084438760b
master <gutislgh@yaho>
2025-06-30 11:14:23 +0300

    Find Поиск и перегрузки

--- first.py
+++ first.py
@@ -1,16 +1,27 @@
 # Строки (immutable, iterable)
-#
-alphab = 'абвгдеёжзийклмнопрстуфхцшщъэюя'
-# alphab_u += alphab.upper()   добавляем Заглавные буквы и тогда надо убрать .lower()
-mess = input('Stroka: ').strip().lower()  # убираем пробелы и делаем все буквы малыми
-key = int(input('key: '))
-crypt = ''
-for lett in mess:    # перебираем каждый символ
-    if lett in alphab:  # проверка символа как буква из алфавита
-        t = alphab.index(lett)   # находим позицию буквы в алфавите
-        newkey = (t + key) % len(alphab) # шифровка Вычисляем новую позицию с учётом сдвига (с остатком от деления)
-        # newkey = (t - key) % len(alphab)  - расшифровка
-        crypt += alphab[newkey]
-    else:
-        crypt += lett
-print('Шифр: ',crypt)
+# Начало и окончание строки startswith endswith
+s = 'Смотреть, вертеть, видеть'
+if s.lower().startswith('смо'):
+    print('Yes')
+if s.endswith('еть'):
+    print('Ye')
+# find('подстрока')
+# find('подстрока',start) поиск с начало строки s с позиции start
+# find('подстрока', 10, 15) поиск с позиции 10 и до позиции 15
+index = s.find('еть')  # возвращает первое появление 'еть' поиск с начала строки s
+print(index)
+s1 = 'синхрофазотрон'
+ch = 'о'
+
+if ch in s1:
+    count = s1.count(ch)
+    print(f'Буква {ch} столько раз {count} в слове {s1}')
+    print(f'position: ', end='')
+    start = 0
+    for i in range(count):
+        pos = s1.find(ch, start)
+        start = pos + 1
+        print(pos)
+
+else:
+    print(f'Буквы {ch} нет в слове {s1}')
