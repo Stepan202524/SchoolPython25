5390ed76b7e44aec2b2ae0c5e9b53891a90752cb
master <gutislgh@yaho>
2025-06-30 11:23:16 +0300

    Replace - замена символов

--- first.py
+++ first.py
@@ -5,14 +5,14 @@ if s.lower().startswith('смо'):
     print('Yes')
 if s.endswith('еть'):
     print('Ye')
+index = s.find('еть')  # возвращает первое появление 'еть' поиск с начала строки s
+print(index)
+
 # find('подстрока')
 # find('подстрока',start) поиск с начало строки s с позиции start
 # find('подстрока', 10, 15) поиск с позиции 10 и до позиции 15
-index = s.find('еть')  # возвращает первое появление 'еть' поиск с начала строки s
-print(index)
 s1 = 'синхрофазотрон'
 ch = 'о'
-
 if ch in s1:
     count = s1.count(ch)
     print(f'Буква {ch} столько раз {count} в слове {s1}')
@@ -22,6 +22,13 @@ if ch in s1:
         pos = s1.find(ch, start)
         start = pos + 1
         print(pos)
-
 else:
     print(f'Буквы {ch} нет в слове {s1}')
+
+# replace('что', 'на что') - полная замена
+# replace('что', 'на что', сколько раз)  - число замен
+s = '+7-012-345-67-89'
+res = ''
+res = s.replace('-',' (', 1)
+res = res.replace('-',') ', 1)
+print(res)
