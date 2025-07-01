a0b278681695e936ff2bcd095f60db90a2f575f3
master <gutislgh@yaho>
2025-07-01 10:33:52 +0300

    Split & Join + Stop Words

--- first.py
+++ first.py
@@ -13,3 +13,18 @@ print(text2)
 text3 = '  P y  t h    o   n   '
 res = ''.join(text3.split())  # убрать все пробелы
 print(res)
+# Фраза: ну я типа вообще короче не понимаю этот язык
+stop_words = ['ну', 'типо', 'короче']
+text = ''
+lst2 = ''
+while (message := input('Сообщение: ')) != '':
+    lst2 = message.split()
+for item in lst2:
+    if item in stop_words:
+        item = ''
+    else:
+        text += item + ' '
+        res = ''.join(text.split())
+print(res)
+for a, b in enumerate(res, 1):
+    print(f'{a}. {b}')
