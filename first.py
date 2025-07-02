056e5eb5b7d611513a198d88a4b1e052d044179e
master <gutislgh@yaho>
2025-07-02 11:49:14 +0300

    Functions - области видимости (глобал & локал)

--- first.py
+++ first.py
@@ -1,36 +1,34 @@
 # Функции
-# Return Value
-def square(num):
-    return num ** 2
-
-                                    # После return функция останавливается (подобно break в цикле)
-def even_odd(num):
-    if num % 2 == 0:
-        return 'Chetnoe'
-    return 'NotCHetnoe'
+# Области видимости
+PI = 3.14       # Глобальная переменная
 
 
-t = square(5)
-print(t)
-
-print(even_odd(10))
-
-num_to_str = {
-    0 : 'null',
-    1 : 'odin',
-    2 : 'dva',
-    11 : 'Odinadc`at`',
-    20 : 'Dvadcat`',
-
-}
-#  Функция, принимающая число и возвращает его словами
-
-def num_to_words(n: int) -> str:
-    if len(str(n)) > 2:
-        return 'Nujno  ne bol`she 2x znakov'
-    if len(str(n)) == 1 or n in num_to_str:
-        return num_to_str[int(n)]
-    return num_to_str[int(str(n)[0] + '0')] + ' ' + num_to_str[int(str(n)[1])]
+def circle_len(radius: float):
+    """
+    :param radius:
+    :return:
+    """
+    perimetr = 2 * PI * radius
+    print(f'Dlina kruga s radiusom {radius} = {perimetr:.3f}')
 
 
-print(num_to_words(22))
+def print_array(array: list) -> None:
+    for item in array:           # Использование внешней(глобальной) переменной Недопустимо (вместо array -- words)
+        print(item)
+
+
+def greet(name: str):
+    print('Privet', name)
+    name = 'drug'
+    print('Zdarova', name)
+
+
+def main():
+    circle_len(3)
+    words = ['privet', 'mir']   # Все переменные в функции - локальные
+    print_array(words)
+    print_array(['a', 'b', 'c'])
+    greet('Fedya')
+
+
+main()      # Вызываем главную функцию и она уже выводит на экран
