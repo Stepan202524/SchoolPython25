cbd28efa5f54378e4ada4d0ed8eac21f0316fbfa
master <gutislgh@yaho>
2025-07-02 15:45:11 +0300

    Аргументы в функции & + or * & Kwargs

--- first.py
+++ first.py
@@ -16,6 +16,7 @@ def fio(name, surname):     # именованные аргументы
 
 print(fio('Ostap', 'Bender'))
 
+
 def calc(*args, oper):
     # match oper:
     #     case '+':
@@ -27,7 +28,6 @@ def calc(*args, oper):
     #         for i in args:
     #             res *= i
     # return res
-
     if oper == '+':
         res = 0
         for arg in args:
@@ -40,3 +40,24 @@ def calc(*args, oper):
         return res
 
 print(calc(1, 2, 4, oper = '+'))
+
+            # Позиционные | Именованные
+def print_any(*args, **kwargs):
+    for i in args:
+        print(i)
+    for k, v in kwargs.items():
+        print(k, '=', v)
+
+print_any(18, 9, name = 'Dima', age =27)
+
+
+def profile(name, surname, city, *child, **dopol):
+    print(f'Imya: {name}')
+    print(f'FIO: {surname}')
+    print(f'Gorod: {city}')
+    if len(child) > 0:
+        print('Dety:', ', '.join(child))
+    # print('Hobby: ', end=': ')
+    print('Hobby:', dopol['hobby'])
+
+profile('Dima', 'Kolesov', 'SPB', 'Petya', 'Oleg', hobby=['Chess', 'Fizra'] )
