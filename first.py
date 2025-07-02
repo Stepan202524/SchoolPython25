1ccaa00b9e0b5167e05c953f5b24806e4a774462
master <gutislgh@yaho>
2025-07-02 15:02:13 +0300

    Аргументы в функции & + or *

--- first.py
+++ first.py
@@ -1,28 +1,42 @@
-# Оператор is: a is b -> когда a и b -один и тот же объект
-my_refr = ['Kolbas', 'Sblr', 'Maslo']
-his_refr = ['Kolbas', 'Sblr', 'Maslo']
-print(my_refr == his_refr)  # Сравнение содержимого объектов
-print(my_refr is his_refr)  # Сравнение объектов
-print(id(my_refr) == id(his_refr))  # Сравнение id-адресов
+# Функция с переменным числом аргументов (с числами)
+def multy(*args):       # Функция выводит в кортеж
+    print(len(args))    # подсчёт числа аргументов (длина)
+    print(args)         # по индексу, либо перебором в цикле (кортеж)
+    res = 1
+    for arg in args:
+        res *= arg      # произведение аргументов
+    return res
+
+# multy(1, 2)
+print(multy(1, 2, 4))
 
 
-def print_array(array: list, start: int = None):
-    if start is None:
-        for i in array:
-            print(i)
+def fio(name, surname):     # именованные аргументы
+    return f'{name} {surname}'
+
+print(fio('Ostap', 'Bender'))
+
+def calc(*args, oper):
+    # match oper:
+    #     case '+':
+    #         res = 0
+    #         for i in args:
+    #             res += i
+    #     case '*':
+    #         res = 1
+    #         for i in args:
+    #             res *= i
+    # return res
+
+    if oper == '+':
+        res = 0
+        for arg in args:
+            res += arg          # Сложение аргументов
+        return res
     else:
-        for i in range(start, len(array)):
-            print(array[i])
+        res = 1
+        for arg in args:
+            res *= arg          # Умножение аргументов
+        return res
 
-
-a = [1, 2, 3]
-print_array(a, 1)
-
-
-# Возврат нескольких значений из функции
-def coord() -> tuple:
-    return 5.4, 3.2, 3.8, 7.2, 9.9
-
-
-x, y, *rest = coord()  # Распаковка (При распаковке * может быть ТОЛЬКО ОДНА)
-print(f'x = {x}, y = {y}, rest = {rest}')  # Остаток rest (в список)
+print(calc(1, 2, 4, oper = '+'))
