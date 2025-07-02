dc7d38c8df3e0e53cb797ffdb33a246b73c03641
master <gutislgh@yaho>
2025-07-02 13:04:54 +0300

    Operator is + None + распаковка + *

--- first.py
+++ first.py
@@ -1,41 +1,28 @@
-# Функции
-# Области видимости
-PI = 3.14       # Глобальная переменная
+# Оператор is: a is b -> когда a и b -один и тот же объект
+my_refr = ['Kolbas', 'Sblr', 'Maslo']
+his_refr = ['Kolbas', 'Sblr', 'Maslo']
+print(my_refr == his_refr)  # Сравнение содержимого объектов
+print(my_refr is his_refr)  # Сравнение объектов
+print(id(my_refr) == id(his_refr))  # Сравнение id-адресов
 
 
-def circle_len(radius: float):
-    """
-    :param radius:
-    :return:
-    """
-    perimetr = 2 * PI * radius
-    print(f'Dlina kruga s radiusom {radius} = {perimetr:.3f}')
+def print_array(array: list, start: int = None):
+    if start is None:
+        for i in array:
+            print(i)
+    else:
+        for i in range(start, len(array)):
+            print(array[i])
 
 
-def print_array(array: list) -> None:
-    for item in array:           # Использование внешней(глобальной) переменной Недопустимо (вместо array -- words)
-        print(item)
+a = [1, 2, 3]
+print_array(a, 1)
 
 
-def greet(name: str):
-    print('Privet', name)
-    name = 'drug'
-    print('Zdarova', name)
+# Возврат нескольких значений из функции
+def coord() -> tuple:
+    return 5.4, 3.2, 3.8, 7.2, 9.9
 
 
-def main():
-    circle_len(3)
-    words = ['privet', 'mir']   # Все переменные в функции - локальные
-    print_array(words)
-    print_array(['a', 'b', 'c'])
-    greet('Fedya')
-
-
-main()      # Вызываем главную функцию и она уже выводит на экран
-
-
-def generate_list():
-    for i in range(5):
-        yield i         # Генератор -- Возвращает значение переменной, но не останавливает
-array = tuple(generate_list())
-print(array)
+x, y, *rest = coord()  # Распаковка (При распаковке * может быть ТОЛЬКО ОДНА)
+print(f'x = {x}, y = {y}, rest = {rest}')  # Остаток rest (в список)
