635364fd0bf504d175d689d94281d5077fa92632
master <gutislgh@yaho>
2025-07-03 11:16:51 +0300

    Анонимные функции - Lambda + Алфавит

--- first.py
+++ first.py
@@ -20,3 +20,26 @@ print(res2, '\n',  res3)
 
 long_word = [word for word in words if len(word) > 4]
 print(long_word)
+
+# Создание алфавитов и их объединение
+ENGLISH_ABC = [chr(ch) for ch in range(ord('a'), ord('z') + 1)]
+RUSS_ABC = [chr(ch) for ch in range(ord('а'), ord('я') + 1)] + ['ё']
+ABC = (set(ENGLISH_ABC) ^set(RUSS_ABC) ^ set([x.upper() for x in ENGLISH_ABC]) ^ set([x.upper() for x in RUSS_ABC]))
+
+# удаление пунктуации из строки
+text = 'ну и зачем, чтобы, если они.!'       # .lower()   если хотим сделать все маленькие буквы
+text = ''.join(filter(lambda x: x in ABC ^ {' '}, text))
+print(text)
+
+def remov_punct(text):
+    return ''.join(filter(lambda x: x in ABC ^ {' '}, text))
+
+print(remov_punct(text))
+
+def get_words(text: str)  -> list:
+    return remov_punct(text).split()
+
+def long_words(text, length=4) -> filter:      # вернёт слова длина которых большеравно 4м
+    return filter(lambda word: len(word) >= length, get_words(text))
+
+print(list(long_words(text)))
