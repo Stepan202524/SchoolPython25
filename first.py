bda234b67fcb620765f0cdcab7b99dac5f5df02c
master <gutislgh@yaho>
2025-07-03 10:39:55 +0300

    Анонимные функции - Lambda

--- first.py
+++ first.py
@@ -1,24 +1,22 @@
-# Функция как объект
-# Если функция передаётся в другие функции, получается: Функция высшего порядка
-
-# Функция критерия отбора элементов списка по длине слова
-def is_longer(word):
-    return len(word) > 4
-
+# Анонимные функции (однострочные, безымянные)
+# lambda  <Аргументы>: <выражение>
 words = ['v','etom', 'spiske', 'slova', 'kotoryx']
-for word in filter(is_longer, words):
-    print(word)
 
+is_long_six = lambda word: len(word) > 6
 
-def if_est_o(word):
-    return word[0] == 's'
-res = list(filter(if_est_o, words))
+if_first_let_a = lambda word: word[0] == 'a'
+
+#def string_contains = lambda s: 'e' in s
+res = list(filter(lambda x: x[0] == 's', words))
 print(res)
 
+res1 = list(filter(lambda s: 'e' in s, words))
+print(res1)
 
-def square(num):
-    return num ** 2
+res2 = list(map(lambda y: y ** 2,range(3, 16)))
+num = [3, 4, 5, 6, 7, 8, 9, 10]
+res3 = [y ** 2 for y in num]
+print(res2, '\n',  res3)
 
-nums = [1, 2, 3, 4, 5, 6]
-squares = map(square, nums)
-print(list(squares))
+long_word = [word for word in words if len(word) > 4]
+print(long_word)
