4e8db8c8e899eccc42c1e19af6e5330c2b084728
master <gutislgh@yaho>
2025-07-04 10:18:41 +0300

    Дом задание(Выборка слов из строк)

--- first.py
+++ first.py
@@ -1,53 +1,18 @@
-# Черепашья графика
-import turtle # as t - Сократим до t, чтобы не писать turtle
+# Встроенные библиотеки
+import sys
 
-turtle.speed(9)
-turtle.penup()
-turtle.goto(-100, -200)
-turtle.pendown()
+strings = [d.strip('\n') for d in sys.stdin.readlines()]
+length = len(strings)  # сколько строк
+rem = length % 3
 
-def tree(leng):
-    if leng < 10:
-        return
-    turtle.forward(leng)
-    turtle.left(30)
-    tree(leng * 0.7)
-    turtle.right(60)
-    tree(leng * 0.7)
-    turtle.left(30)
-    turtle.backward(leng)
+if rem:
+    strings = strings[:length - rem]
 
-turtle.left(90)
-tree(100)
-
-
-N = 5
-colors = ['red', 'green', 'blue', 'orange', 'yellow']
-
-turtle.bgcolor('black')
-angle = 360 // len(colors) - 1
-
-for x in range(100):
-    turtle.pencolor(colors[x % len(colors)])
-    turtle.width(x // 100 + 1)
-    turtle.forward(x)
-    turtle.left(angle)
-
-
-def square(side):
-    for _ in range(5):
-        turtle.forward(side)
-        turtle.left(360 // N)
-
-def circle(radius):
-    for _ in range(5):
-        turtle.circle(radius)
-        turtle.right(360 // N)
-
-circle(60)
-
-for _ in range(N):
-    square(40)
-    turtle.left(360 // 5)
-
-turtle.mainloop()
+for x in range(0, length - rem, 3):
+    summ = sum(len(a) for a in strings[x:x + 3])
+    result = []
+    for s in strings[x:x + 3]:
+        temp = s.lower().split()
+        result += filter(lambda a: len(a) % 2 == summ % 2, temp)
+    result = sorted(set(map(lambda b: b.capitalize(), result)))[:5]
+    print(*result, sep='. ')
