4ca1f715c30e618ed0d92a52958d08ba7f3d60dc
master <gutislgh@yaho>
2025-07-07 11:15:14 +0300

    Docx: Создание и заполнение

--- first.py
+++ first.py
@@ -1,21 +1,29 @@
-from PIL import Image, ImageDraw, ImageFont
-orig = Image.open('Kartinka/sunny_day.jpg').convert('RGB')  # открыть файл и конвертация в RGB
-up = orig.crop((0, 0, 600, 200))        # вырезаем половину картинки
-down = orig.crop((0, 200, 600, 400))    # вырезаем вторую половину
-new =Image.new('RGB', (600, 400))
-new.paste(down, (0, 0))
-new.paste(up, (0, 200))     # Поменяем местами
-#new.show()
+# Документы
+# Word - DOCX (python_docx)
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Cm, Inches, Mm, Pt      # Для размеров
 
-from PIL import ImageFilter, ImageEnhance
+doc = Document()        # Создаём экземпляр документа в оперативной памяти
+doc.add_heading('Otchet',2) # Добавление заголовка
+paragraph = doc.add_paragraph('В этом отчёте есть')     # Создали абзац
+paragraph.add_run('ключевые показатели').bold = True     # Добавляем в абзац текс (run - что-то внутри абзаца
 
-orig1 = Image.open('Kartinka/Spanch.jpg')
-blur_image = orig1.filter(ImageFilter.GaussianBlur(radius=8))        # Размытие
-blur_image.show()
+paragraph = doc.add_paragraph()     # Новый абзац для списка
+paragraph_format = paragraph.paragraph_format
+paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
+paragraph = doc.add_paragraph('Первый пункт', style='List Bullet')  # Маркированный
+paragraph = doc.add_paragraph('Второй пункт', style='List Bullet')
+paragraph = doc.add_paragraph('Первый пункт', style='List Number')  # Нумерованный
+paragraph = doc.add_paragraph('Второй пункт', style='List Number')
+paragraph = doc.add_paragraph()
 
-enchancer = ImageEnhance.Sharpness(orig1)
-sharpened_image = enchancer.enhance(4.0)        # Усиление резкости
-blur_image.show()
+table = doc.add_table(rows=4, cols=4)       #Добавляем таблицу
+for i, row in enumerate(table.rows):        # Заполняем
+    for j, cell in enumerate(table.columns):
+        cell.text = f'Строка {i + 1}, Столбец {j + 1}'
 
-edges = orig1.filter(ImageFilter.FIND_EDGES)    # Контуры
-blur_image.show()
+paragraph = doc.add_paragraph()
+doc.add_picture('Kartinka/Spanch.jpg', width=Mm(105))      # Добавляем картинку в doc
+
+doc.save('docs/report.docx')    # Сохраняем в папку
