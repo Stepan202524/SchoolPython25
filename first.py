122dd77bfae340440fc97dc6c847c872251c0c99
master <gutislgh@yaho>
2025-07-07 10:20:06 +0300

    Картинка с тексом

--- first.py
+++ first.py
@@ -1,31 +1,27 @@
-# pip freeze > requirements.txt     - Создание файла зависимости
-# pip install -r requirements.txt    - установка списка библиотек
-from PIL import Image, ImageDraw       # Подгружаем базовые навыки для картинок, для рисования
+from PIL import Image, ImageDraw, ImageFont
 
+# https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru
+W = 600
+H = 400
+image = Image.new('RGB',
+                  (W, H),
+                  (0, 163, 232))
+draw = ImageDraw.Draw(image)
 
-image = Image.open('Kartinka/Spanch.jpg')
+text = 'Солнечный день'
+# draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
+draw.circle((600, 0), 100, fill='yellow')
+font = ImageFont.truetype(
+    font='fonts/AghelvJ.ttf',  # можно использовать любой установленный шрифт
+    size=50
+)
+# Получаем размеры текста
+_, _, w, h = draw.textbbox((0, 0), text, font=font)
 
-x, y = image.size       # Распаковка размера картинки в координаты (кортеж)
-mode =image.mode        # Режим файла картинки
-pixels = image.load()   # Создать таблицу адресов пикселей  (кортеж - RGB)(двумерный массив)
-for i in range(x):
-    for j in range(y):
-        r, g, b = pixels[i, j]      # Обращение в кортеже
-# image.save('Kartinka/spanch2.jpg')
-print(f'Shyrina = {x}, Vysota = {y}, Color shema = {mode}')
+# Рассчитываем позицию для центрирования
+x = (W - w) // 2
+y = (H - h) // 2
+draw.text((x, y), text, fill=(255, 255, 0), font=font)
 
-# image_rotate = image.rotate(90) # Потом надо сохранять в новый файл, чтоб не испортить оригинал
-# import_flip = image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)    # Отражение
-
-# cropped = image.crop((245, 0, 500, 430))   # Вырезание части картинки (адрес начала - адрес конца)
-# resize = image.resize((x, y)) # Изменение размера
-RED = (255, 0, 0)
-image1 = Image.new('RGB', (600, 400), (0, 0, 255))  # Создание новой картинки с синим фоном
-#image1.save('Kartinka/blue.jpg')
-draw = ImageDraw.Draw(image1)
-draw.line((0, 0, 600, 400), fill=(255, 0 ,0), width=5) # Рисуем линию
-draw.rectangle((10, 10, 590, 390), outline=RED, width=11) # Прямоугольник
-draw.text((150, 150), 'Ura Privet', fill=RED)
-
-POLY = [(100, 50), (150, 50), (180, 120)]
-draw.polygon(POLY, outline='GREEN', width=15) #  Треугольник
+#image.save('Kartinka/sunny_day.jpg')
+image.show()
