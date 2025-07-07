0936af78e257df76065d2fc034c3c577f9e83935
master <gutislgh@yaho>
2025-07-07 16:16:50 +0300

    Файл: ОС-модуль

--- first.py
+++ first.py
@@ -1,47 +1,17 @@
-# Файл
-# name.txt
-# t - текстовый файл(txt, html, xml)
-# b - бинарные файлы(jpg, avi, mp3)
-# w - write (открывается на запись и удаляет все записи в нём, Если не было - создаётся файл
-# a - append (запись в конец)
-# r - read  (чтение, без записи)
+# Файл ОС-модуль
 
-# fo = open('info.txt', 'wt', encoding='utf-8')
-# print(fo)
-# print(fo.mode, fo.name, fo.encoding)
-# count = fo.write('Этот текст будет в файле!')
-# print('В файл записано', count, 'bytes!')
-# fo.close()
+import os
+# os.mkdir('libs')            # Создание директории в корне проекта
+os.makedirs('libs', exist_ok=True)   # Мягкое создание директории
+if os.path.exists('libs'):       # Проверка существования пути
+    os.rmdir('libs')            # Удаление директории
+path = os.getcwd()              # get current working directory
+print(path)
+os.chdir(path + '/fonts')       # Переход в директорию fonts
+print(path)
+# os.chdir('..')                  # Выход на уровень выше (из папки fonts)
+# print(path)
 
-# fo = open('info.txt', 'rt', encoding='utf-8')
-# text = fo.read(11)                    # Сколько байт читать (3) -
-# fo.read(6)                             # Курсор остаётся на месте, после прочтения
-# text += fo.read(7)
-# print('V fayle zapisano:', end=': ')
-# print(text)
-# fo.close()
-
-fo = open('info.txt', 'rt', encoding='utf-8')   # Для добавки текста .write писать 'at'
-# fo.write(' Хороший текст')            # Добавление в конец текста
-# print('\nА вот это будет уже с новой строки.', file=fo)
-# print('Ёще одна строка', file=fo)
-# text = fo.readline()
-# print(text)
-# text = fo.readline()
-# print(text)
-# while text := fo.readline():            # Построчное чтение №1
-#     print(text.rstrip('\n'))
-# lst = fo.readlines()                    # Построчное чтение №2
-# lst = list(map(lambda x: x.strip('\n'), lst))
-# print(lst)
-# text = fo.read()                        # Третий вариант №3
-# lst = text.splitlines()
-# print(lst)
-fo.close
-
-# Открытие с менеджером контекста
-with open('info.txt', 'rt', encoding='utf-8') as fo:
-    text = fo.read()
-    lst = text.splitlines()
-    print(lst)
-# Проследит, чтобы файл закрылся
+all_files = [f for f in os.listdir('.')]  #if f.endswith('.ttf')  # Вывод в список всех файлов директории
+os.chdir('..')
+print(all_files)
