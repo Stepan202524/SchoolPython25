c981d238b5b43fb8ff4837196fb6a2c941e03ff1
master <gutislgh@yaho>
2025-07-07 10:40:50 +0300

    Convert & Filtes

--- first.py
+++ first.py
@@ -1,27 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
+orig = Image.open('Kartinka/sunny_day.jpg').convert('RGB')  # открыть файл и конвертация в RGB
+up = orig.crop((0, 0, 600, 200))        # вырезаем половину картинки
+down = orig.crop((0, 200, 600, 400))    # вырезаем вторую половину
+new =Image.new('RGB', (600, 400))
+new.paste(down, (0, 0))
+new.paste(up, (0, 200))     # Поменяем местами
+#new.show()
 
-# https://fontsforyou.com/ru/specific-fonts/ttf-fonts/languageru
-W = 600
-H = 400
-image = Image.new('RGB',
-                  (W, H),
-                  (0, 163, 232))
-draw = ImageDraw.Draw(image)
+from PIL import ImageFilter, ImageEnhance
 
-text = 'Солнечный день'
-# draw.ellipse((470, -120, 800, 120), outline='yellow', fill='yellow')
-draw.circle((600, 0), 100, fill='yellow')
-font = ImageFont.truetype(
-    font='fonts/AghelvJ.ttf',  # можно использовать любой установленный шрифт
-    size=50
-)
-# Получаем размеры текста
-_, _, w, h = draw.textbbox((0, 0), text, font=font)
+orig1 = Image.open('Kartinka/Spanch.jpg')
+blur_image = orig1.filter(ImageFilter.GaussianBlur(radius=8))        # Размытие
+blur_image.show()
 
-# Рассчитываем позицию для центрирования
-x = (W - w) // 2
-y = (H - h) // 2
-draw.text((x, y), text, fill=(255, 255, 0), font=font)
+enchancer = ImageEnhance.Sharpness(orig1)
+sharpened_image = enchancer.enhance(4.0)        # Усиление резкости
+blur_image.show()
 
-#image.save('Kartinka/sunny_day.jpg')
-image.show()
+edges = orig1.filter(ImageFilter.FIND_EDGES)    # Контуры
+blur_image.show()
