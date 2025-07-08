163597d1b7f74c9426db1ca2ca72d33645f51e9b
master <gutislgh@yaho>
2025-07-08 12:11:20 +0300

    Исключения & delim na 0 & Raise

--- first.py
+++ first.py
@@ -7,21 +7,35 @@
 #    если исключения не было
 # finally:
 #    выполняется в любом случае
-print('Ostatok ot deleniya.')
-loop = True
-while loop:
-    try:
-        value = int(input('Na chto delim 10:'))
-        res = 10 % value
-        print(f'Ostatok ot deleniya 10 na {value} = {res}')
-    except ZeroDivisionError:
-        print('Nelzya delit na 0!')
-    except ValueError:
-        print('Nodo vvodit tolko celye chisla')
-    except Exception as exp:
-        print('Proizoshlo iskluchenie -', exp.__class__.__name__, exp)
-    else:
-        loop = False
+
+# Бросаемся исключениями - raise
+max_val = 10
+min_val = 1
+
+try:
+    val = int(input(f'VVedite celoe chislo ot {min_val} do {max_val}: '))
+    if not min_val < val < max_val:
+        raise ValueError('Vne diapozona')
+    print(f'Chislo {val} lejit v nujnom diapozone')
+except ValueError as exp:
+    print('Vnimatelno: ', exp)
+# print('Ostatok ot deleniya.')
+# loop = True
+# while loop:
+#     try:
+#         value = int(input('Na chto delim 10:'))
+#         res = 10 % value
+#         print(f'Ostatok ot deleniya 10 na {value} = {res}')
+#     except ZeroDivisionError:
+#         print('Nelzya delit na 0!')
+#     except ValueError:
+#         print('Nodo vvodit tolko celye chisla')
+#     except Exception as exp:
+#         print('Proizoshlo iskluchenie -', exp.__class__.__name__, exp)
+#     else:
+#         loop = False
+
+
 # flag = False    # открывался ли на запись
 #
 # try:
