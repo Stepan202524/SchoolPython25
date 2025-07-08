d151d417bf1f4da6378e503f02d55dcde05e61d2
master <gutislgh@yaho>
2025-07-08 15:08:55 +0300

    Морфологический анализ (pymorphy)

--- first.py
+++ first.py
@@ -1,51 +1,20 @@
-# Практикум (обучаемый словарь)
-import pickle
-from fnmatch import translate
+# Библиотека pymorphy
+# pip install pymorphy3
+#pip install -U pymorphy3-dicts-ru
+import pymorphy3
 
-# минимальная версия, если файл dict.dat отсутствует
+morph = pymorphy3.MorphAnalyzer()
+print(morph.parse('Алексей'))
 
-voc = {
-    'стол': 'table',
-    'стул': 'chair',
-    'дверь': 'door'
-}
-# функция распечатки словаря
-def print_voc():
-    print('Soderjanie slovarya: ')
-    for k, v in voc.items():
-        print(k, '-', v)
-# Загружаем словарь из файла
-try:
-    with open('dict.dat', 'rb') as dump_in:
-        voc = pickle.load(dump_in)
-except FileNotFoundError:
-    with open('dict.dat', 'wb') as dump_out:
-        pickle.dump(voc, dump_out)
-    print('Sozdan minimal slovar')
-    print_voc()
-
-while True:
-    temp = input('\nVvedite slovo dlya perevoda or "#" dlya konca: ')
-    word = temp.strip().lower()
-    if word == '#' or word == '№':
-        break
-    if word in voc.keys():
-        translate = voc[word]
-        print(f'Slovo "{word}" perevod: {translate}.\n')
+from pymorphy3 import MorphAnalyzer
+form = MorphAnalyzer().parse('бутылка')[0]
+for btl in reversed(range(11)):
+    print(f'V Holodilnike {btl + 1} {form.make_agree_with_number(btl + 1).word}')
+    print('Voz`mi sebe odnu! ')
+    if btl % 10 == 1 and btl != 11:
+        remain = 'Ostalas`'
     else:
-        print(f'Slovo "{word}" net v slovare.')
-        newkey = f'Kakoy perevod slova "{word}"? \n'
-        newkey += 'Esli net vvoda, Press ENTER '
-        newkey += 'ili vvedite ego tut: '
-        new_word = input(newkey)
+        remain = 'Ostalos`'
+    print(f'{remain} {btl} {form.make_agree_with_number(btl + 1).word}')
 
-        if new_word != '' or len(new_word) > 2:
-            voc[word] = new_word
-            print(f'Slovo {word} s perevodom {new_word} vneseno v slovar')
-        else:
-            print('Nichego ne vvedeno')
-            continue
-# СОхранить словарь
-with open('dict.dat', 'wb') as dump_out:
-    pickle.dump(voc, dump_out)
-print('Bay Bay')
+
