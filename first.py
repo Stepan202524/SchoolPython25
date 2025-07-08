36ef8fe38ec9e41a5ddbad3d433d3b963b7a1224
master <gutislgh@yaho>
2025-07-08 14:39:38 +0300

    Практика (Обучаемый словарь)

--- first.py
+++ first.py
@@ -1,30 +1,51 @@
-# В основном для тестирования ( в prodaction не идёт)
+# Практикум (обучаемый словарь)
+import pickle
+from fnmatch import translate
 
-try:
-    text = input('tekst:')
-    assert len(text) > 3  # утверждение
-except AssertionError:
-    print('Korotkiy text!')
+# минимальная версия, если файл dict.dat отсутствует
 
-lst= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+voc = {
+    'стол': 'table',
+    'стул': 'chair',
+    'дверь': 'door'
+}
+# функция распечатки словаря
+def print_voc():
+    print('Soderjanie slovarya: ')
+    for k, v in voc.items():
+        print(k, '-', v)
+# Загружаем словарь из файла
 try:
-    index = int(input('press index: '))
-    if not -len(lst) < index < len(lst) - 1:
-        raise ValueError('Index vne diapazona')
-    res = lst[index]
-    print(f'Chislo po indexu {index}: {res}')
-except ValueError as exp:
-    print(exp)
+    with open('dict.dat', 'rb') as dump_in:
+        voc = pickle.load(dump_in)
+except FileNotFoundError:
+    with open('dict.dat', 'wb') as dump_out:
+        pickle.dump(voc, dump_out)
+    print('Sozdan minimal slovar')
+    print_voc()
 
 while True:
-    a = input('press 1oe chislo:')
-    b = input('press 2oe chislo:')
-    try:
-        result = int(a) / int(b)
-    except ZeroDivisionError:
-        print('Nel`zya delit` na null!')
-    except ValueError:
-        print('Nujno press chislo')
-    else:
-        print(result)
+    temp = input('\nVvedite slovo dlya perevoda or "#" dlya konca: ')
+    word = temp.strip().lower()
+    if word == '#' or word == '№':
         break
+    if word in voc.keys():
+        translate = voc[word]
+        print(f'Slovo "{word}" perevod: {translate}.\n')
+    else:
+        print(f'Slovo "{word}" net v slovare.')
+        newkey = f'Kakoy perevod slova "{word}"? \n'
+        newkey += 'Esli net vvoda, Press ENTER '
+        newkey += 'ili vvedite ego tut: '
+        new_word = input(newkey)
+
+        if new_word != '' or len(new_word) > 2:
+            voc[word] = new_word
+            print(f'Slovo {word} s perevodom {new_word} vneseno v slovar')
+        else:
+            print('Nichego ne vvedeno')
+            continue
+# СОхранить словарь
+with open('dict.dat', 'wb') as dump_out:
+    pickle.dump(voc, dump_out)
+print('Bay Bay')
