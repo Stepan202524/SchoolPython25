f02e20747ea7ca05abba7d8f4315116113e324c7
master <gutislgh@yaho>
2025-07-08 10:28:36 +0300

    Файл: Сортировка чисел

--- first.py
+++ first.py
@@ -1,17 +1,8 @@
-# Файл ОС-модуль
-
-import os
-# os.mkdir('libs')            # Создание директории в корне проекта
-os.makedirs('libs', exist_ok=True)   # Мягкое создание директории
-if os.path.exists('libs'):       # Проверка существования пути
-    os.rmdir('libs')            # Удаление директории
-path = os.getcwd()              # get current working directory
-print(path)
-os.chdir(path + '/fonts')       # Переход в директорию fonts
-print(path)
-# os.chdir('..')                  # Выход на уровень выше (из папки fonts)
-# print(path)
-
-all_files = [f for f in os.listdir('.')]  #if f.endswith('.ttf')  # Вывод в список всех файлов директории
-os.chdir('..')
-print(all_files)
+res =[]
+with open('info.txt', 'rt') as f:
+    while temp := f.readline():
+        res += temp.split(', ')
+res = list(map(lambda x: x.rstrip('\n'), res))
+#res = set(res)
+res = sorted(int(x) for x in set(res))
+print(res)
