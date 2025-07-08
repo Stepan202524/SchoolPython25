2031f65fa18ec21ab44d76634043c16d806c0785
master <gutislgh@yaho>
2025-07-08 12:01:12 +0300

    Исключения & delim na 0

--- first.py
+++ first.py
@@ -7,19 +7,34 @@
 #    если исключения не было
 # finally:
 #    выполняется в любом случае
-flag = False    # открывался ли на запись
-
-try:
-    fo = open('inform.txt', encoding='utf-8')
-    print(fo.read())
-    fo.close()
-except FileNotFoundError:
-    print('Net takogo fayla')
-    with open('inform.txt', 'wt', encoding='utf-8') as fo:
-        fo.write('По умолчанию')
-else:
-    print('Fayl otkryt uspeshno. Read & close')
-    print(fo.read())
-    fo.close()
-finally:
-    print('Rabotaem dal`she')
+print('Ostatok ot deleniya.')
+loop = True
+while loop:
+    try:
+        value = int(input('Na chto delim 10:'))
+        res = 10 % value
+        print(f'Ostatok ot deleniya 10 na {value} = {res}')
+    except ZeroDivisionError:
+        print('Nelzya delit na 0!')
+    except ValueError:
+        print('Nodo vvodit tolko celye chisla')
+    except Exception as exp:
+        print('Proizoshlo iskluchenie -', exp.__class__.__name__, exp)
+    else:
+        loop = False
+# flag = False    # открывался ли на запись
+#
+# try:
+#     fo = open('inform.txt', encoding='utf-8')
+#     print(fo.read())
+#     fo.close()
+# except FileNotFoundError:
+#     print('Net takogo fayla')
+#     with open('inform.txt', 'wt', encoding='utf-8') as fo:
+#         fo.write('По умолчанию')
+# else:
+#     print('Fayl otkryt uspeshno. Read & close')
+#     print(fo.read())
+#     fo.close()
+# finally:
+#     print('Rabotaem dal`she')
