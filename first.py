194e99540412b3587746473538682e4a1180ffdf
master <gutislgh@yaho>
2025-07-09 11:20:32 +0300

    Регулярные выражения + квантификаторы more + Убираем знаки

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
-# Регулярные выражения (поиск по паттерну)
-# Regular Expressions (re)
+# Регулярные выражения (поиск по паттерну)   regex101.com — отладка RegEx
+# Regular Expressions (re)                      regexr.com — визуализация шаблонов
 # r-строка - raw-string (сырая строка)
 
 # Квантификаторы
@@ -34,7 +34,41 @@ result = re.findall(pattern, test_string)       # Ternary If  Тернарный
 print('Cifry est') if result else print('Netu')
 print(result)
 
-pattern = 'Go{3,}gle'
-test_string = 'Google, Gooogle, Gooooooogle'
-result = re.findall(pattern,test_string)
+# pattern = 'Go{3,}gle'      # Google где 3 и более о
+# test_string = 'Google, Gooogle, Gooooooogle'
+
+# pattern = r'стеклянн?ый'        # 2-я "н" может присутствовать  но не обязана
+# test_string = 'стекляный, стеклянный, оловянный'
+
+# pattern = r'<img.*>'        # "жадный" квантификатор
+# pattern = r'<img.*?>'       # "ленивый" квантификатор
+# pattern = r'<img[^>]+srс="([^">]+)"'        # вытащит bg.jpg только путь к картинке
+# test_string = 'Картинка <img srс="bg.jpg"> в тексте</p>'
+# pattern = r'<p>(.*?)</p>'       # Содержимое абзаца html
+# test_string = '<b>Вот начало: </b><p>Содержимое</p><i>и так далее</i>'
+# pattern = r'<p[^>]*>(.*)</p>'       # Содержимое абзаца html с атрибутами
+# test_string = '<b>Центрируем</b><p align="center">Содержимое</p>'
+# result = re.findall(pattern,test_string)
+# print(result)
+
+# Убираем все знаки препинания
+def remove_punctuation(input_str: str) -> str:
+# Методом sub() заменяем все найденные совпадения пустой строки и возвращаем "очищенную"
+# :param input_str: строка со знаками препинания
+# :return: строку, очищенную от знаков препинания
+    return re.sub(r'[^\w\s]', '', input_str)
+test_string = 'Язык Python, явля?ясь инту,итивно понят.ным'
+result = remove_punctuation(test_string)
 print(result)
+
+# Split
+pattern = r'[,.:;!]'
+test_string = 'яблоко,   груша.   банан  ;  слива  ! абрикос  '
+# test_string = ''.join(test_string.split())      # убрали все пробелы
+result = re.split(pattern, test_string)
+# через map
+# result= list(map(lambda x: x.strip(), result))
+# через list с сортировкой
+result = sorted(x.strip() for x in result)
+print(result)
+
