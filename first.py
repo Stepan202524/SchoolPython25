17d2f4fe17eedab3605c9ddc60dab91a5cdba89a
master <gutislgh@yaho>
2025-07-10 14:10:33 +0300

    OOP + Специальные (Magic) Методы.  Points

--- first.py
+++ first.py
@@ -1,85 +1,27 @@
-# OOP  (Полиморфизм) - свойства кода работать с разными типами данных
-# method override;  operator overloading
-# isinstance (объект, тип) -> True
-# isinstance (объект, (тип 1, тип 2, тип N)) -> True
+# OOP (специальные методы)  magic methods
+# method override, operator overloading
+import math
+from math import hypot
 
-from lib import Book
-book = Book('Язык С++', 'Бьярн')
-print(f'{book.get_title(), book.get_author()}')
-############
-from lib import Circle, Rectangle
-
-
-def shape_info(shape: object):
-    print(f'Plowad {shape.get_name()} : {shape.area()}, Perimetr {shape.get_name()}: {shape.perimetr()}')
-
-
-rect, c = ['Прямоугольника', 'Круга']
-fir = ''
-def shape_info1(shape: object):
-    if isinstance(shape, Circle):
-        fig = c
-    elif isinstance(shape, Rectangle):
-        fig = rect
-    print(f'Plowad` {fig} : {shape.area()}, Perimetr {fig}: {shape.perimetr()}')
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'<Point: ({self.x}, {self.y})>'
+    def __repr__(self):
+        return f'<List Point: ({self.x}, {self.y})>'
+    def __sub__(self, other):
+        return (self.x - other.x, self.y - other.y)
+      #  return Point(abs(self.x - other.x), abs(self.y - other.y))  Абсолютные значения
+    def __add__(self, other):
+        return math.hypot(abs(self.x - other.x), abs(self.y - other.y))
 
 
-r = Rectangle(10, 5)
-shape_info(r)
-cr = Circle(10)
-shape_info(cr)
-shape_info1(r)
-shape_info1(cr)
-###########
-from lib import Person, Student, Employ
-people = [
-    Person('Alexander', 22),
-    Student('Dima', 'GUAP'),
-    Employ('Petya', 'BMW')
-]
-for person in people:
-    if isinstance(person, Student):
-        print(person.get_univer())
-    elif isinstance(person, Employ):
-        print(person.get_company())
-    else:
-        print(person.get_name(), person.get_age())
-############
-lst = list(range(1, 15))
-# Разбиваем список на чётные и нечётные
-class Selector:
-    def __init__(self, vals):
-        self._values = vals[:]      # получаем копию, чтобы не изменять начальный список
-    def get_odd(self):
-        return [x for x in self._values if x % 2] # остаток от деления
-    def get_even(self):
-        return [x for x in self._values if x % 2 == 0]
-
-s = Selector(lst)
-print(f'Nechet: ', s.get_odd())
-print(f' Chet: ', s.get_even())
-print(f'Ves spisok: ', lst)
-#############
-# lst += ['a']
-class Stat:
-    def __init__(self, vals):
-        self._values = vals[:]
-    def is_int(self) -> bool:
-        return all(isinstance(item, int) for item in self._values)
-    def get_min(self):
-        if self.is_int():
-            return min(self._values)
-        return None
-    def get_max(self):
-        if self.is_int():
-            return max(self._values)
-        return None
-    def get_aver(self):
-        if self.is_int():
-            return sum(self._values) / len(self._values)
-        return None
-
-st = Stat(lst)
-print(st.get_min())
-print(st.get_max())
-print(st.get_aver())
+p = Point()
+p1 = [Point(), Point()]
+p2 = Point(5, 8)
+p3 = Point(12, 15)
+print(p, '\n', p1)
+print(f'Raznica koordinat: ', p2 - p3)
+print(f'Gipotinuza: ', p2 + p3)
