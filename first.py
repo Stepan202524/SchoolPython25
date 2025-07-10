a03e44887462aac14c001540ff1a34ef19cba2d7
master <gutislgh@yaho>
2025-07-10 12:28:12 +0300

    OOP + Polymorphism Intro

--- first.py
+++ first.py
@@ -1,139 +1,85 @@
-# OOP  (encapsulation)
-# Свойства классов
-class Fruit:
-    pass
+# OOP  (Полиморфизм) - свойства кода работать с разными типами данных
+# method override;  operator overloading
+# isinstance (объект, тип) -> True
+# isinstance (объект, (тип 1, тип 2, тип N)) -> True
+
+from lib import Book
+book = Book('Язык С++', 'Бьярн')
+print(f'{book.get_title(), book.get_author()}')
+############
+from lib import Circle, Rectangle
 
 
-a = Fruit()
-b = Fruit()
-
-a.name = 'Яблоко'
-a.weigth = 120
-b.name = 'Груша'
-b.weight = 150
-print(a.name, '\t', a.weigth, '\t\t', b.name, b.weight)
-
-# self - это контекстный объект, через который передаётся вызванный метод класса,
-# через него интерпретатор понимает, какой объект вызвал метод
-# self - ссылка ведёт на определённый адрес оперативный памяти,ячейка памяти переменной с именем g
-
-# Методы классов
-
-class Greater:
-    def hello(self, name = 'Noname'):
-        print('Privet!!', name)
-
-    def Bay(self):
-        print('Bay Bay!!', '\n')
+def shape_info(shape: object):
+    print(f'Plowad {shape.get_name()} : {shape.area()}, Perimetr {shape.get_name()}: {shape.perimetr()}')
 
 
-g = Greater()
-g.hello()
-g.Bay()
-
-# Методы классов и анализ предыдущих вызовов
-class Car:
-    counter = 0     # статичное свойство (счётчик)
-
-    def __init__(self, brand='NoName', model='NoName', color='NoName'):
-        self.engine_on = False
-        self._brand = brand    # 'Skoda'
-        self._model = model    #'Oktavia'
-        self._color = color      #'red'
-        Car.counter += 1
-
-    def set_brand(self, new_brand):
-        if new_brand:
-            self._brand = new_brand
-
-    def set_model(self, new_model):
-        if new_model:
-            self._model = new_model
-
-    def set_color(self, new_color):
-        if new_color:
-            self._color = new_color
-
-    def get_brand(self):
-        return self._brand
-
-    def get_model(self):
-        return self._model
-
-    def get_color(self):
-        return self._color
-
-    def start_engine(self):
-        self.engine_on = True
-
-    def drive_to(self, place):
-        if self.engine_on:
-            print(f'Edem v {place} na {self._brand} {self._model} {self._color}')
-        else:
-            print('Ne edem')
-
-    @staticmethod
-    def get_counter():
-        return Car.counter
+rect, c = ['Прямоугольника', 'Круга']
+fir = ''
+def shape_info1(shape: object):
+    if isinstance(shape, Circle):
+        fig = c
+    elif isinstance(shape, Rectangle):
+        fig = rect
+    print(f'Plowad` {fig} : {shape.area()}, Perimetr {fig}: {shape.perimetr()}')
 
 
-car = Car('BMW', '535i', 'black')
-car.start_engine()
-car.drive_to('gorod')
-car1 = Car()
-car2 = Car()
-print('В парке машин:', Car.get_counter(), '\n')
+r = Rectangle(10, 5)
+shape_info(r)
+cr = Circle(10)
+shape_info(cr)
+shape_info1(r)
+shape_info1(cr)
+###########
+from lib import Person, Student, Employ
+people = [
+    Person('Alexander', 22),
+    Student('Dima', 'GUAP'),
+    Employ('Petya', 'BMW')
+]
+for person in people:
+    if isinstance(person, Student):
+        print(person.get_univer())
+    elif isinstance(person, Employ):
+        print(person.get_company())
+    else:
+        print(person.get_name(), person.get_age())
+############
+lst = list(range(1, 15))
+# Разбиваем список на чётные и нечётные
+class Selector:
+    def __init__(self, vals):
+        self._values = vals[:]      # получаем копию, чтобы не изменять начальный список
+    def get_odd(self):
+        return [x for x in self._values if x % 2] # остаток от деления
+    def get_even(self):
+        return [x for x in self._values if x % 2 == 0]
 
-# Геттеры и сеттеры
-class Person:
+s = Selector(lst)
+print(f'Nechet: ', s.get_odd())
+print(f' Chet: ', s.get_even())
+print(f'Ves spisok: ', lst)
+#############
+# lst += ['a']
+class Stat:
+    def __init__(self, vals):
+        self._values = vals[:]
+    def is_int(self) -> bool:
+        return all(isinstance(item, int) for item in self._values)
+    def get_min(self):
+        if self.is_int():
+            return min(self._values)
+        return None
+    def get_max(self):
+        if self.is_int():
+            return max(self._values)
+        return None
+    def get_aver(self):
+        if self.is_int():
+            return sum(self._values) / len(self._values)
+        return None
 
-    def __init__(self, name='Bill', age=11):
-        # свойства (поля) класса
-        self._name = name
-        self._age = age
-
-    # setters
-    def set_name(self, new_name):
-        if new_name:
-            self._name = new_name
-
-    def set_age(self, new_age):
-        if 0 < new_age < 100:
-            self._age = new_age
-        else:
-            print('Net takogo vozrasta', new_age)
-    #getters
-    def get_name(self):
-        return self._name
-
-    def get_age(self):
-        return self._age
-
-    def person_info(self):
-        print(f'Chel s imenem {self._name}. Vozrast: {self._age}')
-
-
-p = Person()
-p.set_age(111)
-p.person_info()
-print(p.get_name(), 'emu stol`ko let', p.get_age())
-
-class Clicker:
-    def __init__(self):
-        self._counter1 = 0
-
-    def click(self):
-        self._counter1 += 1
-
-    def get_counter1(self):
-        return self._counter1
-
-    def reset(self):
-        self._counter1 = 0
-
-cl = Clicker()
-cl.click()
-cl.click()
-print('\n', cl.get_counter1())
-cl.reset()
-print(cl.get_counter1())
+st = Stat(lst)
+print(st.get_min())
+print(st.get_max())
+print(st.get_aver())
--- lib.py
+++ lib.py
@@ -5,3 +5,119 @@ def diff(a, b):
 
 if __name__ == '__first__':
     print('Это библиотека. а исполняемый файл - first')
+
+class Separator:
+    def __init__(self):
+        self.odd = []
+        self.even = []  # чётные
+
+    def add_num(self, num):
+        if num % 2:
+            self.odd.append(num)
+        else:
+            self.even.append(num)
+
+    def get_odd(self):
+        return self.odd
+
+    def get_even(self):
+        return self.even
+
+class Clicker:
+    def __init__(self):
+        self._counter = 0
+
+    def click(self):
+        self._counter += 1
+
+    def get_counter(self):
+        return self._counter
+
+    def reset(self):
+        self._counter = 0
+
+
+class Book:
+    def __init__(self, title, author):
+        self._title = title
+        self._author = author
+
+    def get_title(self):
+        return self._title
+    def get_author(self):
+        return self._author
+
+from math import pi
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.name = 'Kruga'
+
+    def perimetr(self):
+        return round(2 * pi * self.radius, 3)
+
+    def area(self):
+        return round(pi * self.radius ** 2, 3)
+
+    def get_name(self):
+        return self.name
+
+
+class Rectangle:
+    def __init__(self, side1, side2):
+        self.side1 = side1
+        self.side2 = side2
+        self.name = 'Pryamougolnika'
+
+    def perimetr(self):
+        return  self.side1 + self.side2
+
+    def area(self):
+        return self.side1 * self.side2
+
+    def get_name(self):
+        return self.name
+
+
+class Person:
+
+    def __init__(self, name='Bill', age=11):
+        # свойства (поля) класса
+        self._name = name
+        self._age = age
+
+    # setters
+    def set_name(self, new_name):
+        if new_name:
+            self._name = new_name
+
+    def set_age(self, new_age):
+        if 0 < new_age < 100:
+            self._age = new_age
+        else:
+            print('Net takogo vozrasta', new_age)
+    #getters
+    def get_name(self):
+        return self._name
+
+    def get_age(self):
+        return self._age
+
+    def person_info(self):
+        print(f'Chel s imenem {self._name}. Vozrast: {self._age}')
+
+class Student:
+    def __init__(self, name='Bill', univ=''):
+        self._name = name
+        self._univer = univ
+
+    def get_univer(self):
+        return self._name, self._univer
+
+class Employ:
+    def __init__(self, name='BMW', comp=''):
+        self._name = name
+        self._company = comp
+
+    def get_company(self):
+        return self._name, self._company
