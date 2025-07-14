9c9f9e10545a018522e612ba8c8252521c0c41e4
master <gutislgh@yaho>
2025-07-14 14:28:26 +0300

    CSV Dict & Quoting

--- first.py
+++ first.py
@@ -1,17 +1,37 @@
-# Периодические задачи
-import datetime
-import schedule
+# CSV-файлы
+import csv
+# with open('people.csv', 'r', encoding='utf-8') as f:
+#     dict_reader = csv.DictReader(f)
+#     for row in dict_reader:
+#         print(f'{row['name']} jivet v gorode {row['city']}')
 
-i = 1
+# field_names = ['name', 'age', 'city']       # Имена полей
+# data = {
+#     'name' : 'Boris',
+#     'age' : 22,
+#     'city' : 'MSk'
+# }
+# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.DictWriter(f, fieldnames=field_names)
+#     writer.writerow(data)
 
-def job():
-    global  i
-    print(f'Skript zapustilsyz {i} - raz')
-    i += 1
-    t = datetime.datetime.now()
-    print('Vremya', t.strftime('%H:%M:%S'))
+# Режимы квотирования
+data1 = ['name', 35, 'town']
+with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(data1)
 
-schedule.every(5).seconds.do(job)
-
-while True:
-    schedule.run_pending()
+# data = [
+#     ['name', 'age', 'city'],
+#     ['Женя', '46', 'Клн'],
+#     ['Рома', '18', 'СПб'],
+#     ['Павел', '35', 'Мск']
+# ]
+# with open('people.csv', 'r', encoding='utf-8') as f:
+#     reader = csv.reader(f, delimiter=',', quotechar='"')
+#     for row in reader:
+#         print(row)
+#
+# with open('emloyee.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(data)
