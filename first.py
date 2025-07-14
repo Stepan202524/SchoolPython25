32aede398123c1dd2cb1fa6398fa7e3e73d98627
master <gutislgh@yaho>
2025-07-14 15:42:36 +0300

    JSON - Read Write

--- first.py
+++ first.py
@@ -1,18 +1,36 @@
-# Zip
-from zipfile import ZipFile
-import os
-# Архивируем все файлы .csv в Zip
-# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
-# print(csv_files)
-# with ZipFile('archiv.zip', 'w') as myzip:
-#     for file in csv_files:
-#         myzip.write(file)
-#         os.remove(file)
+# JSON - (Java Script Object Notation)
+# Для чтения load() - читает из файла
+#           loads() - читает строковое представление
+import json
 
-# Извлечение файлов из архива
-files_to_exctract = ['people.csv', 'file.csv']
-with ZipFile('archiv.zip', 'r') as zip_obj:
-    zip_obj.extractall(members=files_to_exctract)
-# Получить список файлов в архиве
-with ZipFile('archiv.zip', 'r') as zip_obj:
-    print(zip_obj.namelist())
+with open('dogs.json', 'rt') as d:
+#    data = json.load(d)        # Напрямую из файла
+    temp = d.read()             # Читаем файл как строку
+    data = json.loads(temp)     # строковое представление JSON
+for i in range(len(data)):
+    print(f'Pitomec #{i + 1}')
+    for a, b in data[i].items():
+        if type(b) == list:
+            print(f'\t{a}: {','.join(b)}')
+        else:
+            print(f'{a}: {b}')
+# Для одного питомца вывод
+# for a, b in data.items():
+#     if type(b)  == list:
+#         print(f'{a}: {','.join(b)}')
+#     else:
+#         print(f'{a}: {b}')
+print(data)
+# Записываем словарь в файл JSON
+d = {
+    'ананас': 300,
+    'банан': 400,
+    'яблоко': 150,
+    'груша': 250,
+}
+# Напрямую в файл
+# with open('fruits.json', 'w', encoding='utf-8') as f:
+#     json.dump(d, f, indent=4)
+
+# Вывод в виде строки
+print(json.dumps(d, indent=4))
