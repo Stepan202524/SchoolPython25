5c24eabc8183fc68c25814c5170d98e7966c1c8d
master <gutislgh@yaho>
2025-07-14 14:43:47 +0300

    Zip-Архивирование

--- first.py
+++ first.py
@@ -1,37 +1,18 @@
-# CSV-файлы
-import csv
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     dict_reader = csv.DictReader(f)
-#     for row in dict_reader:
-#         print(f'{row['name']} jivet v gorode {row['city']}')
+# Zip
+from zipfile import ZipFile
+import os
+# Архивируем все файлы .csv в Zip
+# csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+# print(csv_files)
+# with ZipFile('archiv.zip', 'w') as myzip:
+#     for file in csv_files:
+#         myzip.write(file)
+#         os.remove(file)
 
-# field_names = ['name', 'age', 'city']       # Имена полей
-# data = {
-#     'name' : 'Boris',
-#     'age' : 22,
-#     'city' : 'MSk'
-# }
-# with open('file.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.DictWriter(f, fieldnames=field_names)
-#     writer.writerow(data)
-
-# Режимы квотирования
-data1 = ['name', 35, 'town']
-with open('sample.csv', 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerow(data1)
-
-# data = [
-#     ['name', 'age', 'city'],
-#     ['Женя', '46', 'Клн'],
-#     ['Рома', '18', 'СПб'],
-#     ['Павел', '35', 'Мск']
-# ]
-# with open('people.csv', 'r', encoding='utf-8') as f:
-#     reader = csv.reader(f, delimiter=',', quotechar='"')
-#     for row in reader:
-#         print(row)
-#
-# with open('emloyee.csv', 'w', newline='', encoding='utf-8') as f:
-#     writer = csv.writer(f)
-#     writer.writerows(data)
+# Извлечение файлов из архива
+files_to_exctract = ['people.csv', 'file.csv']
+with ZipFile('archiv.zip', 'r') as zip_obj:
+    zip_obj.extractall(members=files_to_exctract)
+# Получить список файлов в архиве
+with ZipFile('archiv.zip', 'r') as zip_obj:
+    print(zip_obj.namelist())
