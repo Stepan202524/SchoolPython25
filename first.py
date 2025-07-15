084e977f332cbee68c65c5594e93fa8d61072d6e
master <gutislgh@yaho>
2025-07-15 15:39:26 +0300

    Функция (переопределение)

--- first.py
+++ first.py
@@ -1,31 +1,16 @@
-# Погода через API
-import requests
-from PIL import Image
-import io
+# Декораторы
+def answer(question):
+    return 'Dumay!'
 
-API_KEY = '087a95ccd5be51143083f162cdf078d3'
-URL = 'http://api.openweathermap.org/data/2.5/weather'
-CITY = 'Выборг'
-params = {
-    'q': CITY,
-    'appid': API_KEY,
-    'units': 'metric',
-    'lang': 'ru'
-}
-response = requests.get(URL, params=params)
-result = response.json()
+def dialog():
+    def answer(question):
+        if question.lower().startswith('kogda'):
+            return 'nikogda'
+        else:
+            return 'Upppss'
+    question = input()
+    while question != '':
+        print(answer(question))
+        question = input()
 
-weather = result['weather'][0]['description']
-temper = result['main']['temp']
-humid = result['main']['humidity']
-wind = result['wind']['speed']
-data = result['coord']
-ll = f'{data['lon']}, {data['lat']}'
-
-print(f'Koordinates: {ll}')
-print(f'Segodnya v gorode {CITY}: {weather}', '\t', f'Temper: {temper:1f}', '\n', f'Vlajnost: {humid}%', '\t',
-      f'Speed: {wind} M/C')
-link = f'https://static-maps.yandex.ru/1.x/?ll={ll}&spn=0.0025,0.0025&l=map&pt={ll},pm2dgl'
-image = requests.get(link).content
-if image:
-    Image.open(io.BytesIO(image)).show()
+dialog()
