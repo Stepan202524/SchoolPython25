97cfbd6eca1133014e0a59334176c8d352c34d1b
master <gutislgh@yaho>
2025-07-15 14:21:59 +0300

    SQL: class Crud

--- first.py
+++ first.py
@@ -1,47 +1,48 @@
-# Базы данных (чтение)
-"""
-1. Импорт библиотеки sqlite3
-2. Подключаемся к БД
-3. Назначить курсор
-4. Работа с БД (запросы ответы)
-5. Отключаем БД
-"""
 import sqlite3
-import csv
+from venv import create
 
-connection = sqlite3.connect('db/movies.sqlite')
-cursor = connection.cursor()
-# Добавляем из csv-файла
-with open('people.csv', 'r', encoding='utf=8') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)    # Пропустить первую строку
-    for name, age in reader:
-        cursor.execute(
-            """
-            insert into users(name, age) values(?,?)
-            """, (name, int(age))
+
+class Crud:
+    def __init__(self, db_path):
+        self._conn = sqlite3.connect(db_path)
+        self._cur = self._conn.cursor()
+
+    def create(self,table_name, name, age):
+        res = self._cur.execute(
+            f'insert into {table_name}(name, age) values (?, ?)'
+            ,(name, int(age))
         )
+        self._conn.commit()
 
-# result = cursor.execute(
-#     """
-#     select title, year from films where year = 2010
-#     """
-# )
-# # fetchall - Всё    fetchone - Только первое соответствие   fetchmany(N) - N-соответствий
-# array = result.fetchall()
-# print(array)
-# for title, year in array:
-#     print(title, year)
-# connection.close()
+# Метод чтения таблицы
+    def read(self, table_name):
+        res = self._cur.execute(
+             f""" select * from {table_name}"""
+        ).fetchall()
+        for num, name, age in res:
+            print(num, name, age)
 
-# Запись в БД
-# 4.5 Подтвердить изменения (commit)
-# connection = sqlite3.connect('db/movies.sqlite')
-# cursor = connection.cursor()
-# result = cursor.execute(
-#     """
-#     insert into users(name, age) values('Marks', 55)
-#     """
-# )
-connection.commit()
-connection.close()
+    def update(self, table_name, id_num, name=None, age=None):
+        self._cur.execute(
+            f'update {table_name} set name="{name}", age={age} where id={id_num}'
+        )
+        self._conn.commit()
+
+# Метод удаления строки по id из таблицы
+    def delete(self, id_num, table_name):
+        res = self._cur.execute(
+            f'delete from {table_name} where id={id_num}'
+        )
+        self._conn.commit()
+
+# Отключаем курсор и БД (уничтожение объекта из памяти)
+    def __del__(self):
+        self._cur.close()
+        self._conn.close()
+        print('Object kills')
+
+db = Crud('db/movies.sqlite')
+# db.delete(3, 'users')
+db.create('users', 'Dima', 18)
+db.update('users', 8, 'Kostya', 44 )
+db.read('users')
