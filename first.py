112ef4c7839248824e56ec721510a445e544a099
master <gutislgh@yaho>
2025-07-15 15:51:03 +0300

    Функция (переопределение)

--- first.py
+++ first.py
@@ -1,16 +1,30 @@
 # Декораторы
-def answer(question):
-    return 'Dumay!'
+def upper_case_print(old_func):
+    def new_func(*args, **kwargs):
+        case = kwargs.pop('case', None)
+        if case == 'U':
+            args = [str(arg).upper() for arg in args]
+        elif case == 'L':
+            args = [str(arg).lower() for arg in args]
+        return old_func(*args, **kwargs)
+    return new_func
 
-def dialog():
-    def answer(question):
-        if question.lower().startswith('kogda'):
-            return 'nikogda'
-        else:
-            return 'Upppss'
-    question = input()
-    while question != '':
-        print(answer(question))
-        question = input()
+new_print = upper_case_print(print)
+new_print('Privet, Poka')
+new_print('PRIVet, pOKA', case='L')
 
-dialog()
+# def answer(question):
+#     return 'Dumay!'
+#
+# def dialog():
+#     def answer(question):
+#         if question.lower().startswith('kogda'):
+#             return 'nikogda'
+#         else:
+#             return 'Upppss'
+#     question = input()
+#     while question != '':
+#         print(answer(question))
+#         question = input()
+#
+# dialog()
