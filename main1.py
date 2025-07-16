f3df597a51c26d3906d1b34f85b83bdcff555758
master <gutislgh@yaho>
2025-07-16 11:48:51 +0300

    Sample Page HTML

--- main1.py
+++ main1.py
@@ -1,6 +1,8 @@
 # Flask
 # MVC - (Model View Controller)
-from flask import Flask
+from fileinput import filename
+
+from flask import Flask, url_for
 
 app = Flask(__name__)
 @app.route('/')
@@ -12,11 +14,32 @@ def index():
 def about():
     print('Funkciya about')
     return 'O nas'
+
 @app.route('/countdown')
 def cd():
     lst = [str(x) for x in reversed(range(10))]
     lst.append('Poleteli!!')
     return '<br>'.join(lst)
+
+@app.route('/image')
+def show_image():
+    return f'<img src="{url_for('static', filename='images/spanch.jpg')}">'
+                     # static/images/spanch.jpg">')
+
+@app.route('/sample-page')
+def sample_page():
+    return f"""
+            <!DOCTYPE html>
+        <html lang="ru">
+        <head>
+            <meta charset="UTF-8">
+            <title>Kartinka</title>
+        </head>
+        <body>
+            <img src="{url_for('static', filename='images/spanch.jpg')}" alt="Spanch"> 
+        </body>
+        </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
