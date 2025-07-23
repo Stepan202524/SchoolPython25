fca411ded0e12149df487c610eec8826834f7947
master <gutislgh@yaho>
2025-07-23 10:55:57 +0300

    тест2 API Resources

--- main1.py
+++ main1.py
@@ -28,7 +28,8 @@ from werkzeug.utils import secure_filename
 from forms.loginform import LoginForm
 from forms.news import NewsForm
 from forms.user import Register
-from data import db_session, news_api
+from data import db_session, news_api, api_resources
+from flask_restful import Api
 from data.users import User
 from data.news import News
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
@@ -36,6 +37,7 @@ from flask_login import LoginManager, login_user, logout_user, current_user, log
 from send_mail import send_mail
 
 app = Flask(__name__)
+api = Api(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -353,6 +355,10 @@ def mail_send():
 if __name__ == '__main__':
     db_session.global_init('db/news.sqlite')
     app.register_blueprint(news_api.blueprint)
+    # Доступ к отдельной новости
+    api.add_resource(api_resources.NewsResource, '/api/v2/news/<int:news_id>')
+    # Доступ ко всем новостям
+    api.add_resource(api_resources.NewsResourceList, '/api/v2/news')
     app.run(host='localhost', port=5000)
 
     # news = News()
